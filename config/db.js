096568f7b00071c6d7973e8976d3d39644178fa6
aakanxamodha <aakanxa.ganglia@gmail.com>
2025-01-22 12:06:06 +0530

    MongoDb Atlas Connection Configuration

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const dbURI = "mongodb+srv://sprintly-ganglia:sprintly-ganglia0601@sprintly-ganglia.w1fqw.mongodb.net/?retryWrites=true&w=majority&appName=sprintly-ganglia"; 
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB Atlas");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); 
+  }
+};
+
+module.exports = connectDB;
