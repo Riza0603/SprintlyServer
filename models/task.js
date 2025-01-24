75a2b2e5a86b72fda9755fc9c78b6c68ae15e63a
aakanxamodha <aakanxa.ganglia@gmail.com>
2025-01-24 11:43:30 +0530

    Added fields according to the frontend design.

--- models/task.js
+++ models/task.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    status: String, 
-    createdAt: { type: Date, default: Date.now },
+    title: { type: String, required: true }, // Title field
+    description: { type: String, required: true }, // Description field
+    projectName: { type: String, default: "None" }, // Project name
+    assignee: { type: String, default: "Unassigned" }, // Assignee
+    status: { type: String, default: "No Progress" }, // Status
+    priority: { type: String, default: "None" }, // Priority
+    startDate: { type: Date }, // Start Date
+    endDate: { type: Date }, // End Date
+    createdAt: { type: Date, default: Date.now }, // Auto-set creation date
 });
 
 const TaskModel = mongoose.model('Task', TaskSchema);
