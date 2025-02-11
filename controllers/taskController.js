28eb8ff3729a1763eefbb2e9e19c04a22bebe6f3
Riza0603 <rizafarooq39@gmail.com>
2025-02-11 10:11:30 +0530

    header for notificationsetting

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -12,6 +12,7 @@ export const addTask = async (req, res) => {
       priority: req.body.priority || "None",
       startDate: req.body.startDate || null,
       endDate: req.body.endDate || null,
+      createdBy: req.body.createdBy || null,
       comments: req.body.comments || [],
     };
 
