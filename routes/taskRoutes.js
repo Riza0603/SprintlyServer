868710cca7cc6ace71bb689f5a957db102b1c40a
ReshmaGanglia <reshma.ganglia@gmail.com>
2025-02-11 16:20:36 +0530

    api for updating status

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addComment, addTask, getComments, getTasks } from "../controllers/taskController.js";
+import { addComment, addTask, getComments, getTasks, updateStatus } from "../controllers/taskController.js";
 
 const router = express.Router();
 
@@ -8,4 +8,5 @@ router.post("/tasks", addTask); // Add Task
 router.get("/tasks", getTasks); // Fetch Tasks
 router.post("/addComment/:taskId",addComment);//add comments
 router.post("/getComments/:taskId",getComments);
+router.post("/updateStatus",updateStatus)
 export default router;
