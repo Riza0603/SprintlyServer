0e9eadde4f742eb234c48e3c0537a90913ea3a65
Shashank <shashank.ganglia@gmail.com>
2025-04-19 10:38:58 +0530

    project report generation

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createProject,fetchProjectData, fetchProjects,getProjectFiles,updateGlobalSettings, updateProjectDeletedFile, updateProject,updateProjectSettings, getMembers, deleteMember, addMember,  deleteUser, getProjectByName, fetchDetails, fetchProjectsById, getProjectByManager, updateProjectStatus } from "../controllers/projectController.js";
-
+import { generateReport } from "../controllers/projectreport.js";
 const router = express.Router();
 
 router.post("/createProject", createProject);
@@ -22,5 +22,6 @@ router.post("/getProjectFiles/:projectId",getProjectFiles);router.get("/getProje
 
 router.post("/getProjectFiles/:projectId",getProjectFiles);
 router.post("/updateProjectStatus/:projectName",updateProjectStatus);//update project status
+router.get("/generate-pdf/:projectName", generateReport);
 
 export default router;
