98d90a51880a3d8c7483be27171483552f409160
Ankit-Ganglia <ankit.ganglia@gmail.com>
2025-04-19 11:04:42 +0530

    m

--- models/Notification.js
+++ models/Notification.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const notificationSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, 
-  type: { type: String, enum: ["Task", "Project", "Request","CommentMention","TaskUpdate","ProjectRemoval"], required: true }, 
+  type: { type: String, enum: ["Task", "Project", "Request","CommentMention","TaskUpdate","ProjectRemoval","AdminAccess","ProjectDeletion"], required: true }, 
   message: { type: String, required: true }, // Notification message
   entity_id: { type: mongoose.Schema.Types.ObjectId, required: true }, // ID of the related task/project/request
   metadata: { type: Object, default: {} }, // Dynamic field for additional info
